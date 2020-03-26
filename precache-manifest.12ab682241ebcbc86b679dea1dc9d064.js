125629f73ce561d58c0921b0df5feff12f637346
shel-luc <51696132+shel-luc@users.noreply.github.com>
2020-03-26 18:22:59 -0400

    Updates

---
+++ precache-manifest.12ab682241ebcbc86b679dea1dc9d064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260c19a9f7a88765415d865c98faf7a4",
+    "url": "/ReactCalc/index.html"
+  },
+  {
+    "revision": "2f66a2a9e9a35ba1c3e6",
+    "url": "/ReactCalc/static/css/main.f10d924b.chunk.css"
+  },
+  {
+    "revision": "c573c4dcac5a13c6de19",
+    "url": "/ReactCalc/static/js/2.d38d35af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCalc/static/js/2.d38d35af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f66a2a9e9a35ba1c3e6",
+    "url": "/ReactCalc/static/js/main.550dd3ea.chunk.js"
+  },
+  {
+    "revision": "5a231178e4af3da13f0f",
+    "url": "/ReactCalc/static/js/runtime-main.c8ed93bc.js"
+  }
+]);
